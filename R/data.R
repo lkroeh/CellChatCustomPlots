20b6f87e8ff866c312ca6790f5925cfc3a0c0786
lkroeh <lkroeh@bu.edu>
2026-04-30 17:14:01 -0400

    Add example data and data documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example CellChat object list
+#'
+#' A list of 5 slimmed CellChat objects for use in examples and tests.
+#' Each object contains a subset of cell types and signaling pathways.
+#'
+#' @format A named list of 5 CellChat objects
+"example_CellChat_list"
+
+#' Example CellChat database
+#'
+#' The CellChat ligand-receptor database extracted from the example objects.
+#' Store separately and re-attach to objects when running extract_cellchat_data.
+#'
+#' @format A list containing the CellChat DB slot
+"example_DB"
