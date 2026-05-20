753b09a78aded763644a0faedafa9fe3509a5608
Lina Kroehling <lina@dot1x-nat-10-222-63-12.bumc.bu.edu>
2026-05-20 13:01:59 -0400

    Add differential heatmap plotting functions

--- R/prephm.R
+++ R/prephm.R
@@ -2,6 +2,8 @@
 #'
 #' @param df.netPx1outgoing A data frame with columns source, target,
 #'   pathway_name, and prob containing CellChat communication probabilities.
+#' @param min_rowsum Numeric row-sum cutoff used to remove pathways with low
+#'   total communication probability.
 #'
 #' @return A data frame with pathways as rows and cell types as columns,
 #'   filtered for meaningful interactions and ordered by decreasing row sums.
@@ -9,7 +11,7 @@
 #' @importFrom reshape cast
 #' @importFrom stats na.omit
 #' @export
-prephm <- function(df.netPx1outgoing) {
+prephm <- function(df.netPx1outgoing, min_rowsum = 0.05) {
   dfoutgoing <- reshape::cast(df.netPx1outgoing, pathway_name~source, value = 'prob', fun.aggregate = 'sum')
   dfincoming <- reshape::cast(df.netPx1outgoing, pathway_name~target, value = 'prob', fun.aggregate = 'sum')
   
@@ -20,7 +22,7 @@ prephm <- function(df.netPx1outgoing) {
   dfall1 <- dfall[,-1]
   
   dfall2 <- stats::na.omit(dfall1)
-  pt1 <- which(rowSums(dfall2) >= 0.05)
+  pt1 <- which(rowSums(dfall2) >= min_rowsum)
   dfall3 <- dfall2[pt1,]
   
   dfall5 <- dfall3[order(-rowSums(dfall3)),]
